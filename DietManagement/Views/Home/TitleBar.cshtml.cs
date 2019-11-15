481a3e05197acdadc192ac9075b8fcaf63bc31ae
蔡逸叡 <as860215@hotmail.com>
2019-11-15 23:31:05 +0800

    初始化設定

---
+++ DietManagement/Views/Home/TitleBar.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DietManagement.Views.Home
+{
+    public class TitleBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
