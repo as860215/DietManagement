83552b079ebb05c1e47ac1d4f0c0739e21730c42
蔡逸叡 <as860215@hotmail.com>
2019-12-02 00:06:38 +0800

    設定檔

--- DietManagement/DBConnect.cs
+++ DietManagement/DBConnect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace DietManagement
 {
     public class DBConnect
     {
-        private const string sqlCommand = @"Data Source=DESKTOP-AIS9VBL\SQLEXPRESS;Initial Catalog=DietManagement;User ID=capoo;Pwd=capoo";
+        private string sqlCommand;
 
         public SqlConnection conn { get; set; }
 
@@ -17,6 +18,10 @@ namespace DietManagement
         /// </summary>
         public DBConnect()
         {
+            using (var sr = new StreamReader("setting.config"))
+            {
+                sqlCommand = sr.ReadToEnd();
+            }
             conn = new SqlConnection(sqlCommand);
             conn.Open();
         }
