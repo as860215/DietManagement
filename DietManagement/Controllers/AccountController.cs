adb80ca450f399b97f808ca683562e15390bb80a
蔡逸叡 <as860215@hotmail.com>
2019-12-03 23:08:02 +0800

    login

---
+++ DietManagement/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DietManagement.Models;
+
+namespace DietManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// 登入
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            return PartialView("");
+        }
+
+        /// <summary>
+        /// 登出
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Logout()
+        {
+            return PartialView("");
+        }
+
+        /// <summary>
+        /// 註冊
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Registered()
+        {
+            return PartialView("");
+        }
+
+        /// <summary>
+        /// 使用者資訊
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult User()
+        {
+            return PartialView("");
+        }
+    }
+}
